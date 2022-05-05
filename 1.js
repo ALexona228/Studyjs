668cc99eb811d21fed5cbc22e388b34dd6df4db3
Alexona228 <alalovsyo@gmail.com>
2022-05-05 12:22:29 +0300

    sokr svoistva

---
+++ 1.js
@@ -0,0 +1,4 @@
+let a = 1;
+let b = { toString() {return '1'} };
+let c = 1;
+console.log(a+c+b)
---
+++ shortName.js
@@ -0,0 +1,8 @@
+const name='Bogdan'
+const postsQty=23
+const userPofile={
+    name,
+    postsQty,
+    hasSignedAgreement:false
+}
+console.log(userPofile)
