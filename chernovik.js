807c86a6be6e9f9d6e317f62e93013d37bc853fc
ALexona228 <alalovsyo@gmail.com>
2022-05-16 15:07:14 +0300

    chernovik

---
+++ chernovik.js
@@ -0,0 +1,4 @@
+const myFunction= function(value, multiplier=1) {
+    return value*multiplier
+}
+console.log(myFunction(3))
---
+++ shablonnie_stroki.js
@@ -0,0 +1,4 @@
+const myName= 'Alexei'
+const iLeave= 'Odessa'
+const info= `${myName} ${iLeave}`
+console.log(info)
